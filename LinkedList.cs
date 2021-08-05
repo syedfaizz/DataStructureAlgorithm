d75f89cc9b602cb2436f571a254e9a1f2d2c7b2b
syedfaizz <syedfaizz45@gmail.com>
2021-08-05 18:40:32 +0530

    [faisal] Add: uc4-insert-at-position

--- LinkedList.cs
+++ LinkedList.cs
@@ -45,7 +45,39 @@ namespace LinkedList
                 Console.Write(temp.data + "->");
                 temp = temp.next;
             }
-            Console.Write("null");
+            Console.Write("null \n");
+        }
+        /// <summary>
+        /// insert element at specified postion in linkedlist.
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            Node newestNode = new Node(data);
+            if (this.head == null)
+            {
+                return newestNode;
+            }
+            if (position == 0)
+            {
+                newestNode.next = this.head;
+                this.head = newestNode;
+                return this.head;
+            }
+            Node prev = null;
+            Node current = this.head;
+            int count = 0;
+            while (current != null && count < position)
+            {
+                prev = current;
+                current = current.next;
+                count++;
+            }
+            newestNode.next = prev.next;
+            prev.next = newestNode;
+            return this.head;
         }
     }
 }

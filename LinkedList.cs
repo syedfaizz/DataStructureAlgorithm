16611ded6fdfc50f9d27fd92b1ba80708efc08f9
syedfaizz <syedfaizz45@gmail.com>
2021-08-05 18:22:09 +0530

    [faisal] Add: uc2-add-element

--- LinkedList.cs
+++ LinkedList.cs
@@ -4,43 +4,45 @@ using System.Text;
 
 namespace LinkedList
 {
-        public class LinkedList
+    class LinkedList
+    {
+        // variables
+        internal Node head;
+        /// <summary>
+        /// add element into reverse order.
+        /// </summary>
+        /// <param name="data"></param>
+        internal void AddElementInReverse(int data)
         {
-            internal Node head;
-
-            internal void Add(int data)
+            Node newNode = new Node(data);
+            if (this.head == null)
             {
-                Node node = new Node(data);
-                if (this.head == null)
-                {
-                    this.head = node;
-                }
-                else
-                {
-                    Node temp = head;
-                    while (temp != null)
-                    {
-                        temp = temp.next;
-                    }
-                    temp.next = node;
-                }
-                Console.WriteLine("{0} inserted into Linked List," + node.data);
+                this.head = newNode;
             }
-
-            internal void Display()
+            else
             {
-
                 Node temp = this.head;
-                if (temp == null)
-                {
-                    Console.WriteLine("LinkedList is empty");
-                    return;
-                }
-                while (temp != null)
-                {
-                    Console.WriteLine(temp.data + " ");
-                    temp = temp.next;
-                }
+                head = newNode;
+                head.next = temp;
             }
         }
+        /// <summary>
+        /// display the elements of linkedlist.
+        /// </summary>
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + "->");
+                temp = temp.next;
+            }
+            Console.Write("null");
+        }
     }
+}
---
+++ Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class Node
+        {
+            // variables
+            public int data;
+            public Node next;
+            /// <summary>
+            /// constructor is used to initialized the data.
+            /// </summary>
+            /// <param name="data"></param>
+            public Node(int data)
+            {
+                this.data = data;
+            }
+        }
+    }
